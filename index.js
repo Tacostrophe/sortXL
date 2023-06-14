6422eef05b24b51693b6a480b75a6471d4d52402
Дмитрий Кутин <TacoKong@yandex.ru>
2023-06-15 00:15:52 +0300

    work in progress

--- index.js
+++ index.js
@@ -3,22 +3,108 @@ import * as fs from 'fs';
 import * as readline from 'readline';
 
 const args = process.argv;
-const fileName = args[2] ?? 'file.txt';
+const fileDir = 'files';
+const inputFileName = args[2] ?? (fileDir + '/input.txt');
+const outputFileName = args[3] ?? (fileDir + '/output.txt');
+const tmpDir = `${fileDir}/tmp`;
+const tmpName = (num) => (`${tmpDir}/tmp_${num}.txt`);
+const tmpFiles = [];
+const getFileSize = (path) => fs.lstatSync(path).size;
+const chunkSize = 64 * 1024 * 1024;
 
-async function processLineByLine() {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(fileName),
-    crlfDelay: Infinity,
-  });
+async function splitAndSort() {
+  const lines = [];
+  let partOfLine = '';
+  let reader, writer;
+  const fileSize = getFileSize(inputFileName);
+  for (let i = 0; (i * chunkSize) < fileSize; i++) {
+    reader = readline.createInterface({
+      input: fs.createReadStream(inputFileName, {
+        start: i * chunkSize,
+        end: (i + 1) * chunkSize - 1,
+        encoding: 'utf-8',
+      }),
+      crlfDelay: Infinity,
+    });
 
-  rl.on('line', (line) => {
-    console.log(`line frome file: ${line}`);
-  });
+    reader.on('line', (line) => {
+      lines.push(line);
+    });
 
-  await events.once(rl, 'close');
+    await events.once(reader, 'close');
 
-  const mbUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-  console.log(`Used ${Math.round(mbUsed * 100) / 100 mb}`);
+    lines[0] = partOfLine + lines[0];
+    // console.log(lines[0]);
+    // console.log(lines[0].length);
+    if (((i + 1) * chunkSize) < fileSize) {
+      partOfLine = lines.pop();
+    }
+
+    lines.sort();
+
+    writer = fs.createWriteStream(tmpName(i));
+    tmpFiles.push(tmpName(i));
+    writer.write(lines.join('\n'));
+    lines.length = 0;
+
+  }
 }
 
-processLineByLine();
+async function mergeFiles() {
+  if (tmpFiles.length === 1) {
+    fs.rename(tmpFiles[0], outputFileName, function (err) {
+      if (err) throw err;
+    });
+    return;
+  }
+
+  let firstFileSize, firstReader, firstFileName;
+  let secondFileSize, secondReader, secondFileName;
+  let tempFile;
+  const sortedArr = [];
+  let i = 0;
+  let jFirst = 0;
+  let jSecond = 0;
+  const firstArr = [];
+  const secondArr = [];
+  let writer;
+  const tmpSumName = (num) => `${tmpDir}/tmpsum_${num}.txt`;
+  const tmpSumCounter = 0;
+  while (tmpFiles.length > 1) {
+    firstFileName = tmpFiles[i];
+    secondFileName = tmpFiles[i+1]
+    writer = fs.createWriteStream(tmpSumName(tmpSumCounter++), {flags: 'a'});
+    firstFileSize = getFileSize(firstFileName);
+    secondFileSize = getFileSize(secondFileName);
+
+    while (
+      (jFirst * chunkSize / 2) < firstFileSize ||
+      (jSecond * chunkSize / 2) < secondFileSize
+      ) {
+      firstReader = readline.createInterface({
+        input: fs.createReadStream(firstFileName, {
+          start: jFirst * chunkSize / 2,
+          end: (jFirst + 1) * chunkSize / 2 - 1,
+          encoding: 'utf-8',
+        })
+      });
+      secondReader = readline.createInterface({
+        input: fs.createReadStream(secondFileName, {
+          start: jSecond * chunkSize /2,
+          end: (jSecond + 1) * chunkSize / 2 - 1,
+          encoding: 'utf-8',
+        })
+      })
+      
+    }
+    i = (i > (tmpFiles.length - 3)) ? 0 : (i + 2);
+  }
+}
+
+(async function () {
+  await splitAndSort();
+  console.log(tmpFiles);
+  // console.log(process.memoryUsage());
+  const mbUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(`Used ${Math.round(mbUsed * 100) / 100} Mb`);
+})();
